2e1e42f27a5bfd0ccf220c939bc76ac777b4820d
Delputte Maarten <maarten.delputte@outlook.com>
2022-10-26 23:02:15 +0200

    update the default config json location

--- config/config.js
+++ config/config.js
@@ -1,9 +1,16 @@
 import nconf from "nconf";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the current file path
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name where this file resides
+const __dirname = path.dirname(__filename);
 
 // Get all default config from the config file
 nconf.file(
   {
-    file: `${process.cwd()}/config.json`
+    file: `${__dirname}/../config.json`
   }
 );
 
